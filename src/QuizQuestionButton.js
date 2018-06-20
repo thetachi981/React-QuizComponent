d4ad452b9e0dad171bd98842b6ec31c533fcfeec
Michael Howell <michaelhowellrt@hotmail.com>
2018-06-20 11:52:33 -0400

    Quiz question button

---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+
+class QuizQuestionButton extends Component {
+render (){
+    return (
+        <li><button/></li>
+    )
+}
+}
+export default QuizQuestionButton
