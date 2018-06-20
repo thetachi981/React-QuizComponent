d34f5422ab1e20f9769c145f1806c560a65b903f
Michael Howell <michaelhowellrt@hotmail.com>
2018-06-20 09:26:38 -0400

    question position fix

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,7 +11,7 @@ class Quiz extends Component {
     render (){
     return (
         <div>
-            <QuizQuestion quiz_question = {quizData.quiz_question[this.state.quiz_position]-1}/>
+            <QuizQuestion quiz_question = {quizData.quiz_question[this.state.quiz_position-1]}/>
         </div>
 
         )
