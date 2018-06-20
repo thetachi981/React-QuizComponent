6ace0f46a3586698ed0241a257b748116519b1af
Michael Howell <michaelhowellrt@hotmail.com>
2018-06-20 09:19:20 -0400

    removed extra div

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,7 +11,7 @@ class Quiz extends Component {
     render (){
     return (
         <div>
-            <div className="QuizQuestion"><QuizQuestion quiz_question = {quizData.quiz_question[this.state.quiz_position]-1}/></div>
+            <QuizQuestion quiz_question = {quizData.quiz_question[this.state.quiz_position]-1}/>
         </div>
 
         )
